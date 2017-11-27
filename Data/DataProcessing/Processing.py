4be8526a088964bc890670d7ec8c2caded7c13ee
mkumaszka <martyna.kumaszka18@gmail.com>
2017-11-27 17:37:49 +0100

    Update Processing.py
    removed print

--- Data/DataProcessing/Processing.py
+++ Data/DataProcessing/Processing.py
@@ -13,7 +13,6 @@ def process_raw_data(raw_data):
 if __name__ == '__main__':
     user_logs = pd.read_csv(path.join(config.config['DATA']['data_folder'], 'user_logs.csv'), nrows =3)
     preprocessed_user_logs = process_raw_data(user_logs)
-    print(preprocessed_user_logs)
     with open('user_logs_processed.pkl', "wb") as file:
         pkl.dump(preprocessed_user_logs, file)
 
